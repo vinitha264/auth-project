c7e3cab98b91c09d7b2d4797ca060691e29d5280
Vinitha <vinithabairagoni@gmail.com>
2026-03-04 12:06:00 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,7 @@
-// config/db.js
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI); // ✅ no extra options
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB connected");
     } catch (err) {
         console.error("❌ MongoDB connection error:", err);
