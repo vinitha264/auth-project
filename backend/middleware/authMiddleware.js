4e8902476ffb220310dc2ce4b50029ee2cd54613
Vinitha <vinithabairagoni@gmail.com>
2026-03-04 12:07:17 +0530

    Remove comment from authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,3 @@
-// middleware/authMiddleware.js
 const isAuthenticated = (req, res, next) => {
     if (req.isAuthenticated()) return next();
     res.status(401).json({ message: "Not Authorized" });
