cce33063f7e385a9b96758a23f7125a37c4090d9
Vinitha <vinithabairagoni@gmail.com>
2026-03-07 12:53:53 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:"",
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["auth-project-20.onrender.com"]
+  }
+});
